51d23339f2d9e748fa035539bcef18d235f6396b
Zak-devz <zakariarassili@gmail.com>
2024-01-29 17:05:26 +0100

    final version

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,22 +1,23 @@
-'use strict';
-import '../css/main.css';
 const $ = require('jquery');
+// Load the full build
 const _ = require('lodash');
+import '../css/main.css';
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+const $button = $('<button>Click here to get started</button>').on(
+  'click',
+  _.debounce(updateCounter, 500, { leading: true, trailing: false })
+);
 
 $('body').append("<div id='logo'></div>");
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append($button);
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
-
-const updateCounter = () => {
-  let times = $('#count').html() || 0;
-  $('button').on('click', () => {
-    times++;
-    $('#count').html(`${times} clicks on the button`);
-  });
-};
-
-_.debounce(updateCounter, 500);
-updateCounter();
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: path.resolve(__dirname, 'js/dashboard_main.js'),
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
   performance: {
     maxAssetSize: 1000000,
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
@@ -17,14 +17,14 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(jpg|gif|png|jpeg|svg)$/i,
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
           'file-loader',
           {
             loader: 'image-webpack-loader',
             options: {
-              bypassOnDebug: true,
-              disable: true,
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
           },
         ],
