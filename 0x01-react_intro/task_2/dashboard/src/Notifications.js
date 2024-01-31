a1f9141fbed5ba95a8f77388f9ff903854c1d283
Zak-devz <zakariarassili@gmail.com>
2024-01-31 15:02:42 +0100

    task done

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -5,6 +5,7 @@ export default function Notifications() {
   return (
     <div className='Notifications'>
       <p>Here is the list of notifications</p>
+      <button style={{ float: 'absolute', top: '2px', right: '2px' }}>x</button>
     </div>
   );
 }
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -3,4 +3,7 @@ const getFullYear = () => new Date().getFullYear();
 const getFooterCopy = (isIndex) =>
   isIndex ? `Holberton School` : `Holberton School main dashboard`;
 
-export { getFullYear, getFooterCopy };
+const getLatestNotification = () =>
+  `<strong>Urgent requirement</strong> - complete by EOD`;
+
+export { getFullYear, getFooterCopy, getLatestNotification };
